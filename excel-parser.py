2551952bae26ac4ca46545f057d7116eb253c4ac
dreamxinxcode <dreamxinxcode@gmail.com>
2019-11-07 14:44:21 +0000

    writes properly

--- excel-parser.py
+++ excel-parser.py
@@ -1,19 +1,28 @@
 import openpyxl
 import os
 
-directory = os.path.dirname(os.path.realpath(__file__))
-target = input("Target string: ")
-replace = input("Replacment string ")
+DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+target = input("\033[1m\033[96mTarget string: \033[0m")
+replace = input("\033[1m\033[96mReplacment string: \033[0m")
 
-for files in os.walk(directory):
+for (root, dirs, files) in os.walk(DIRECTORY):
     for file in files:
         if file.endswith(".xlsx"):
-            print(file)
-            wb = openpyxl.load_workbook(file)
-            for sheet in wb.worksheets:
-                for cell in sheet.iter_rows('C{}:C{}'.format(sheet.min_row,sheet.max_row)):
-                    print(cell + "\n")
-                    if cell.value == target:
-                        cell.value = replace
+            path = os.path.join(root, file)
+            print(
+                "\033[1m\033[96mOpening:\033[0m \033[1m\033[93m{0}\033[0m".format(path))
+            wb = openpyxl.load_workbook(path)
+            ws = wb.active
+            for ws in wb.worksheets:
+                for row in ws.iter_rows():
+                    for cell in row:
+                        # print(cell.value)
+                        if cell.value == target:
+                            print("\033[1m\033[96mTARGET STRING FOUND\033[0m")
+                            print("\033[1m\033[96mReplacing\033[0m \033[1m\033[93m{0}\033[0m with \033[1m\033[93m{1}\033[0m".format(
+                                target, replace))
+                            cell.value = replace
 
-    wb.save(wb)
+            print(
+                "\033[1m\033[96mSaving:\033[0m \033[1m\033[93m{}\033[0m".format(file))
+            wb.save(file)
