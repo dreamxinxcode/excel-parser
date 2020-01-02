20603b041b7ed6f39551d64070e53f4694a1cd0d
dreamxinxcode <dreamxinxcode@gmail.com>
2020-01-02 12:48:34 -0800

    day

--- template.py
+++ template.py
@@ -1,6 +1,26 @@
-from openpyxl import Workbook
+import openpyxl
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, colors
-import datetime
+from datetime import datetime
+import time
+import os
+
+
+def list_all(file):
+        start_time = time.time()
+        print(
+            "\033[1m\033[96mOpening:\033[0m \033[1m\033[93m{0}\033[0m".format(file))
+        wb = openpyxl.load_workbook(file, data_only=True)
+        ws = wb.active
+        for ws in wb.worksheets:
+            for row in ws.iter_rows():
+                for cell in row:
+                    if (cell.value != None):
+                        print(cell.value)
+
+
+        print(
+            "\033[1m\033[96mSaving:\033[0m \033[1m\033[93m{}\033[0m in \033[1m\033[96m{} seconds\033[0m\n".format(file, round((start_time - time.time()), 2)))
+        wb.save(file)
 
 
 def template():
@@ -9,16 +29,19 @@ def template():
 
     model = ws['A2']
     model.font = Font(color=colors.RED, bold=True, size=26, name='Calibri')
-    model.value = "Boat Moddel"
+    model.value = "Boat Model"
 
     # Aluminum
     ws['B3'] = "Aluminum"
     ws['B3'].font = Font(name='Calibri', size=16, bold=True)
-    header_items = ["Quantity", "Item Description", "Supplier Part #", "Supplier Name", "Cost (EACH)", "Cost (TOTAL)"]
+    header_items = ["Quantity", "Item Description", "Supplier Part #",
+                    "Supplier Name", "Cost (EACH)", "Cost (TOTAL)"]
     header = ws.append(header_items)
     header.font = Font(name="Calibri", size=11, bold=True, italic=True)
 
     wb.save('template.xlsx')
 
+
 if(__name__ == "__main__"):
-    template()
+    file = input("File name: ")
+    list_all(file)
